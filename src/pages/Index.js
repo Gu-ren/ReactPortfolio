7f9ce6d82dec5039713588f35c21044d1a46373d
Gu-ren <114053482+Gu-ren@users.noreply.github.com>
2022-12-18 01:34:00 +0800

    second commit

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
-import Home from './Home'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 const index = () => {
   return (
     <div>
       <Router>
         <Routes>
-            <Route exact path='/' element={<Home/>}></Route>
-           
+          <Route exact path="/ReactPortfolio" element={<Home />}></Route>
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
