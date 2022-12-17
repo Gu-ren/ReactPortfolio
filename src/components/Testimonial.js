e83dd3973a5f2624baa1d0c760cfd4880f4f2c5b
Gu-ren <114053482+Gu-ren@users.noreply.github.com>
2022-12-18 01:47:44 +0800

    fourth

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -38,13 +38,15 @@ const Testimonial = () => {
           position="KodeGo Instructor"
         /> */}
         <Card
-          img="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-1/275060766_10217514878458700_8903523126286898553_n.jpg?stp=dst-jpg_p320x320&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEFrgSQB2zhugyfpf30_hZ1Ikz1BKbDf4IiTPUEpsN_gljil5MnThZZUSTWh6Qbg9lXVWOlL7U-mtcxAs8YD5Yi&_nc_ohc=N3YaoMdrrRcAX-1pFfY&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfA_0u3OgsFxCpnzuPpocrmbML2C1942yAp0q0rnmJwADw&oe=639CEFEB"
+          img="/ReactPortfolio
+/          https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-1/275060766_10217514878458700_8903523126286898553_n.jpg?stp=dst-jpg_p320x320&_nc_cat=108&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeEFrgSQB2zhugyfpf30_hZ1Ikz1BKbDf4IiTPUEpsN_gljil5MnThZZUSTWh6Qbg9lXVWOlL7U-mtcxAs8YD5Yi&_nc_ohc=N3YaoMdrrRcAX-1pFfY&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfA_0u3OgsFxCpnzuPpocrmbML2C1942yAp0q0rnmJwADw&oe=639CEFEB"
           text='"Working with Glenn on our  Capstone project was a great experience. He understands instructions very well, writes good code and always asks relevant questions. He would be a great addition to your team"'
           name="Danil Cristales"
           position="KodeGo Teamate"
         />
         <Card
-          img="https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-1/285805368_3104744556457593_3057326052926961798_n.jpg?stp=dst-jpg_s320x320&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeF2kULP1BXqY_hKUjj4Qgvu1ZjfRbj1NoTVmN9FuPU2hOsUehuSqjhHxMNgaicaxikEgqzO848Ub7tK6FrPv6Ok&_nc_ohc=Qj4XlQtGZzsAX_puffB&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfDOBstCE8I8OJtgkcXmDsLZkNcZNFnDSe2rm2_BbjmVfA&oe=639CABDA"
+          img="/ReactPortfolio
+/          https://scontent.fdvo2-1.fna.fbcdn.net/v/t39.30808-1/285805368_3104744556457593_3057326052926961798_n.jpg?stp=dst-jpg_s320x320&_nc_cat=103&ccb=1-7&_nc_sid=7206a8&_nc_eui2=AeF2kULP1BXqY_hKUjj4Qgvu1ZjfRbj1NoTVmN9FuPU2hOsUehuSqjhHxMNgaicaxikEgqzO848Ub7tK6FrPv6Ok&_nc_ohc=Qj4XlQtGZzsAX_puffB&_nc_ht=scontent.fdvo2-1.fna&oh=00_AfDOBstCE8I8OJtgkcXmDsLZkNcZNFnDSe2rm2_BbjmVfA&oe=639CABDA"
           text='"Having glenn as a teammate on our capstone project is great. He is creative and a hard worker that gives new ideas for the project."'
           name="Alex Pacaldo"
           position="KodeGo Teamate"
